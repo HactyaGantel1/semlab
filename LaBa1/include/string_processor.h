a7f249553f27f28b7e9da847ba3029c265dfbb49
HactyaGantel1 <zxchactya@gmail.com>
2025-11-06 00:40:34 +0300

    Create string_processor.h

---
+++ LaBa1/include/string_processor.h
@@ -0,0 +1,10 @@
+#pragma once
+
+
+void processCharArray(char str[]);
+void processCString(char* str);
+
+
+bool isWordDelimiter(char c);
+bool containsDigit(const char* start, const char* end);
+void reverseWord(char* start, char* end);
